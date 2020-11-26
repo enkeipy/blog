eda2de71ac60de71facabb27bb5b8c3525621a4e
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-25 19:54:02 -0600

    Add: bootstrap4 package, basic nav, index.html. Hide create/delete buttons with is_auth...

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import HomePage
 
 urlpatterns = [
+    path('', HomePage.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls'))
 ]
