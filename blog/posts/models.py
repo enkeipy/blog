abf47609777cfbeea6aedf21e143d13c455faa99
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-25 22:32:07 -0600

    Add creation_date field to Post model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.urls import reverse
-
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(default=timezone.now)
 
     def get_absolute_url(self):
         return reverse('posts:detail', kwargs={'pk': self.pk})
