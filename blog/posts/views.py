45e704f7d9599c384da3c74e3ff8b496aa80fd6d
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-25 22:37:45 -0600

    Add LoginRequiredMixin to Post CUD

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class PostList(ListView):
@@ -11,17 +12,17 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'text']
 
 
-class PostUpdate(UpdateView):
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'text']
+    fields = ['title', 'text', 'creation_date']
     template_name_suffix = '_update_form'
 
 
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('all')
