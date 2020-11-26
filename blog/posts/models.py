f8473e60b90db923801baf5d6933697520fe2356
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-26 10:34:22 -0600

    Fix field type for text and path for img placeholder

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    text = models.CharField(max_length=100)
+    text = models.TextField()
     creation_date = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='images/', default='media/images/placeholder.jpg')
+    image = models.ImageField(upload_to='images/', default='images/placeholder.jpg')
 
     def get_absolute_url(self):
         return reverse('posts:detail', kwargs={'pk': self.pk})
