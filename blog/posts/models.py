d9be9d2c49b4f8d1e70068e6a164dee88ab88e68
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-27 14:25:34 -0600

    Handle blank post image in templates

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -6,7 +6,10 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     creation_date = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='images/', default='images/placeholder.jpg')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('posts:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return f'{self.title}'
