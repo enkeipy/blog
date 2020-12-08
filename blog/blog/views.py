1b87d4710dd6b56c48fa0750cde57c3bfb827cf4
Nikita Kukshinski <nkstatedev@gmail.com>
2020-12-07 23:03:34 -0600

    Separate view for Home. move h1 for pages to template head block

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
-
+from django.views.generic import ListView
 from posts.views import PostList
+from posts.models import Post
 
-class HomePage(PostList):
-    pass
+class HomePage(ListView):
+    model = Post
+    paginate_by = 6
+    template_name = 'index.html'
