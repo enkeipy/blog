f565e3fc74076024ead61e108da130094e839fa7
Nikita Kukshinski <nkstatedev@gmail.com>
2020-12-14 22:16:09 -0600

    New Home Page with About and Links sections

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,10 @@
 from posts.models import Post
-from posts.views import PostDetailView
+from django.views.generic.base import TemplateView
 
-class HomePage(PostDetailView):
-    model = Post
-    def get_object(self):
-        return Post.objects.first()
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = Post.objects.first()
+        return context
